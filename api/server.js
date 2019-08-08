a608e2b15a4fbbc7b94cd8b7febed4782d0b45ef
Richard Scott Prins Jr <richardsprins@gmail.com>
2019-08-08 10:18:39 -0700

    adding JOTD

--- api/server.js
+++ api/server.js
@@ -3,7 +3,10 @@ const express = require('express')
 const server = express();
 
 server.get('/', (req, res) => {
-  res.status(200).json({ api: 'running' })
+  res.status(200).json({ 
+    api: 'running',
+    jargonOfTheDay: process.eventNames.JOTD
+   })
 })
 
 module.exports = server
