21a292a6c6da60dfea69a9019e0a762b5823d1a9
Richard Scott Prins Jr <richardsprins@gmail.com>
2019-08-08 10:22:17 -0700

    change

--- api/server.js
+++ api/server.js
@@ -5,7 +5,7 @@ const server = express();
 server.get('/', (req, res) => {
   res.status(200).json({ 
     api: 'running',
-    jargonOfTheDay: process.eventNames.JOTD
+    jargonOfTheDay: process.env.JOTD
    })
 })
 
